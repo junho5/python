b6b7757098c7066b088e306e787b84896570a822
junho5 <dhwnsghdhkd@naver.com>
2021-08-20 23:33:31 +0900

    test02 update

--- test02.py
+++ test02.py
@@ -16,7 +16,22 @@ def calculate_change(payment, cost):
     thousand = (charge-(charge%1000))/1000
     charge -= thousand*1000
     print("1000원 지폐: {:.0f}장".format(thousand))
-    
+
+# 파이썬의 버림 나눗셈 // 요자식을 이용해서 해봄
+def calculate_change(payment, cost):
+    change = payment - cost  # 거스름돈 총액
+
+    fifty_count = change // 50000  # 50,000원 지폐
+    ten_count = (change % 50000) // 10000  # 10,000원 지폐
+    five_count = (change % 10000) // 5000  # 5,000원 지폐
+    one_count = (change % 5000) // 1000  # 1,000원 지폐
+
+    # 답 출력
+    print("50000원 지폐: {}장".format(fifty_count))
+    print("10000원 지폐: {}장".format(ten_count))
+    print("5000원 지폐: {}장".format(five_count))
+    print("1000원 지폐: {}장".format(one_count))
+
 
 
 # 테스트
