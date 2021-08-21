f8c9f3d444071d0ec3c1409b2058f0ce81cebad9
junho5 <dhwnsghdhkd@naver.com>
2021-08-22 00:41:44 +0900

    test organize

--- if_test04.py
+++
@@ -1,9 +0,0 @@
-N = 120
-i = 1
-cnt = 0
-while i<=N:
-    if N % i == 0:
-        cnt += 1
-        print(i)
-    i += 1
-print("{}의 약수는 총 {}개입니다.".format(N,cnt))
--- test01.py
+++ test01.py
@@ -1,3 +1,7 @@
+# 어떤 수가 짝수인지 홀수인지 판단해 주는 함수 is_evenly_divisible를 쓰세요.
+# is_evenly_divisible는 number(수)를 파라미터로 받습니다. 짝수인 경우, 즉 number가 2로 나누어 떨어질 경우에는 True를 리턴해 줍니다. 홀수인 경우, 즉 number가 2로 나누어 떨어지지 않을 경우에는 False를 리턴해 줍니다.
+# 함수 안에는 print문이 아닌, return문을 사용해야 합니다. 그리고 참고로 불린 개념을 잘 사용하면, 함수 단 한 줄로 작성할 수 있습니다!
+
 def is_evenly_divisible(number):
     result = None
     if(number%2==0):
