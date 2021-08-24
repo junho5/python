8e03c60f87eb1f179f1cc6277fae720bcadd53ec
junho5 <dhwnsghdhkd@naver.com>
2021-08-24 22:22:31 +0900

    list

---
+++ list.py
@@ -0,0 +1,14 @@
+# 리스트 (list)
+
+numbers = [2, 3, 5, 7, 11, 13]
+names = ["윤수", "혜린", "태호", "영훈"]
+
+# 인덱싱 (indexing)
+print(names[0])
+print(numbers[1]+numbers[3])
+print(numbers[-2])
+
+# 리스트 슬라이싱 (list slicing)
+print(numbers[2:]) # 2부터 마지막
+print(numbers[:2]) # 0 ~ 1 까지임 실수 조심
+print(numbers[2:5]) # 2 ~ 4까지임 실수 조심
