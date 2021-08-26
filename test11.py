3f5b3232051d8c2ac2ce8b7ae851a3c2bded68b6
junho5 <dhwnsghdhkd@naver.com>
2021-08-27 00:17:51 +0900

    test 11

---
+++ test11.py
@@ -0,0 +1,10 @@
+numbers = [2, 3, 5, 7, 11, 13, 17, 19]
+
+# 리스트 뒤집기
+# 코드를 입력하세요.
+for i in range(len(numbers)//2):
+    right = len(numbers) - i - 1
+    numbers[right], numbers[i] = numbers[i],numbers[right]
+
+
+print("뒤집어진 리스트: " + str(numbers))
