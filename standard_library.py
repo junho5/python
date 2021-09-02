dbb7670807fc4f7fe4c25899a1c1a4cc4fb5a5fc
junho5 <dhwnsghdhkd@naver.com>
2021-09-02 14:31:15 +0900

    standard_library

---
+++ standard_library.py
@@ -0,0 +1,16 @@
+import math
+
+print(math.log10(100))
+print(math.cos(0))
+print(math.pi)
+
+
+import random
+
+print(random.random()) # 0.0 ~ 1.0 사이 랜덤 숫자
+
+
+import os
+print(os.getlogin())
+print(os.getcwd())
+
