91f8eaf5e2c1e56962e443fafc68a897d40ab9f4
junho5 <dhwnsghdhkd@naver.com>
2021-09-02 14:39:04 +0900

    random

---
+++ random_library.py
@@ -0,0 +1,11 @@
+import random
+
+print(random.randint(1,20)) # 1 ~ 20 사이 숫자 랜덤 발생
+print(random.randint(1,20))
+print(random.randint(1,20))
+print(random.randint(1,20))
+
+print(random.uniform(0,1)) # 0 ~ 1 사이 소수 랜덤 발생
+print(random.uniform(0,1))
+print(random.uniform(0,1))
+print(random.uniform(0,1))
