7df092eb12e1116aeb2674993eca7fce0b09ce36
junho5 <dhwnsghdhkd@naver.com>
2021-09-04 23:23:57 +0900

    test20

---
+++ test20.py
@@ -0,0 +1,9 @@
+with open('vocabulary.txt','r')as f:
+    problem = []
+    for i in f:
+        data = i.strip().split(': ')
+        answer = input(data[1]+': ')
+        if answer == data[0]:
+            print('맞았습니다 !\n')
+        else:
+            print('아쉽습니다. 정답은 {}입니다.'.format(data[0]))
