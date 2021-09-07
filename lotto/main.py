76eab94450309e8891b05200eb414f605050a58e
junho5 <dhwnsghdhkd@naver.com>
2021-09-07 23:09:24 +0900

    lotto2

--- lotto/main.py
+++ lotto/main.py
@@ -1,11 +1,15 @@
 from random import randint
 
 
+from random import randint
+
+
 def generate_numbers(n):
     list = []
     for i in range(n):
         number = randint(1,45)
-        list.append(number)
+        if number not in list:
+            list.append(number)
     return list
 
 print(generate_numbers(6))
