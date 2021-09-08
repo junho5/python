41eda27802caa9f42d3c61e375adabbd6120bb06
junho5 <dhwnsghdhkd@naver.com>
2021-09-08 23:23:46 +0900

    update lotto

--- alias.py
+++ alias.py
@@ -4,13 +4,13 @@ y = 3
 print(x)
 print(y)
 
-x = [2,3,5,7,11]
+x = [2, 3, 5, 7, 11]
 y = x
 y[2] = 999
 print(x)
 print(y)
 
-x = [2,3,5,7,11]
+x = [2, 3, 5, 7, 11]
 y = list(x)
 y[2] = 999
 print(x)
--- lotto/main.py
+++ lotto/main.py
@@ -4,7 +4,7 @@ from random import randint
 def generate_numbers(n):
     list = []
     for i in range(n):
-        number = randint(1,45)
+        number = randint(1, 45)
         if number not in list:
             list.append(number)
     return list
@@ -13,6 +13,6 @@ def generate_numbers(n):
 def draw_winning_numbers():
     winning_numbers = generate_numbers(7)
     return sorted(winning_numbers[:6]) + winning_numbers[6:]
-    
-    
+
+
 print(draw_winning_numbers())
