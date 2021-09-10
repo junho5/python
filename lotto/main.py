4f7c93bd078e2a690b6bd72c4c375d2f45824787
junho5 <dhwnsghdhkd@naver.com>
2021-09-10 23:38:28 +0900

    lotto update

--- lotto/main.py
+++ lotto/main.py
@@ -23,7 +23,3 @@ def count_matching_numbers(list_1, list_2):
     return cnt        
 
 
-# 테스트
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [2, 11, 13, 14, 30, 35]))
-print(count_matching_numbers([2, 7, 11, 14, 25, 40], [14]))
-print(draw_winning_numbers())
