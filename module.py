e8487561ce588f83a80a1d4bf1667711fc20d7a5
junho5 <dhwnsghdhkd@naver.com>
2021-09-11 23:22:16 +0900

    test01

--- module.py
+++ module.py
@@ -1,9 +1,8 @@
 # import calculator  # 같은 폴더에 있을때 하는 방법
-import calculator as cal # 이름 설정
-from calculator import add, multiply # 특정 함수만 가져오기 앞에 함수이름 안써도 가능
-from calculator import * # 다 가져오기
+import calculator as cal  # 이름 설정
+from calculator import add, multiply  # 특정 함수만 가져오기 앞에 함수이름 안써도 가능
+from calculator import *  # 다 가져오기
 
 
-
-print(cal.add(3,4))
-print(add(3,4))
+print(cal.add(3, 4))
+print(add(3, 4))
