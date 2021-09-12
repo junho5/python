74623b59a5693f0788c798b1879d76953d453a03
junho5 <dhwnsghdhkd@naver.com>
2021-09-12 22:26:13 +0900

    lotto finish

--- lotto/main.py
+++ lotto/main.py
@@ -20,6 +20,36 @@ def count_matching_numbers(list_1, list_2):
         for j in range(len(list_2)):
             if list_1[i]==list_2[j]:
                 cnt += 1
-    return cnt        
+    return cnt       
+
+def count_matching_numbers(numbers, winning_numbers):
+    cnt = 0
+    for i in range(len(numbers)):
+        for j in range(len(winning_numbers)):
+            if numbers[i]==winning_numbers[j]:
+                cnt += 1
+    return cnt  
+
+
+def check(numbers, winning_numbers):
+    count = count_matching_numbers(numbers, winning_numbers[:6])
+    bonus_count = count_matching_numbers(numbers, winning_numbers[6:])
+    if count == 6:
+        return 1000000000
+    elif count ==5 and bonus_count ==1:
+        return 50000000
+    elif count ==5:
+        return 1000000
+    elif count == 4:
+        return 50000
+    elif count == 3:
+        return 5000
+    else:
+        return 0
+
+
+# 테스트
+print(check([2, 4, 11, 14, 25, 40], [4, 12, 14, 28, 40, 41, 6]))
+print(check([2, 4, 11, 14, 25, 40], [2, 4, 10, 11, 14, 40, 25])) 
 
 
