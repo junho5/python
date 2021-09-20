1c1b29faade8eebd1634ff73776ea64cc16e83b1
junho5 <dhwnsghdhkd@naver.com>
2021-09-20 23:21:03 +0900

    baseball 04

--- baseball/main.py
+++ baseball/main.py
@@ -12,18 +12,20 @@ def generate_numbers():
     print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
     return numbers
 
+
 def take_guess():
     print("숫자 3개를 하나씩 차례대로 입력하세요.")
-    
+
     new_guess = []
-    while len(new_guess)<3:
+    while len(new_guess) < 3:
         num = int(input("{}번째 숫자를 입력하세요".format(len(new_guess+1))))
-        if num<0 or num>9:
+        if num < 0 or num > 9:
             print('범위를 벗어나는 숫자입니다. 다시 입력하세요.')
         elif num in new_guess:
             print('중복')
         else:
             new_guess.append(num)
     return new_guess
+
 
 print(take_guess())
